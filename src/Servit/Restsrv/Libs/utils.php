eae1da64cf02255afa17c99f416ea9c950e52717
limweb@hotmail.com <limweb@hotmail.com>
2018-02-11 02:44:17 +0700

    getallheader fixed

--- src/Servit/Restsrv/Libs/utils.php
+++ src/Servit/Restsrv/Libs/utils.php
@@ -152,3 +152,15 @@ function nonce(){
   $nonce = microtime() . rand(0,100000);
   return $nonce;
 }
+
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+    $headers = [];
+    foreach ($_SERVER as $name => $value) {
+        if (substr($name, 0, 5) == 'HTTP_') {
+            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        }
+    }
+    return $headers;
+    }
+}
